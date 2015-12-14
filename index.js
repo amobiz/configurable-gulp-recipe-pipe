430ed230ab3000779ae178e48642eeb64d889027
amobiz <amobiz.tw+github@gmail.com>
2015-12-15 02:38:45 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var helper = require('configurable-gulp-helper');
+var verify = helper.verifyStreamPrerequisite('pipe');
+
+/**
+ * Recipe:
+ * 	Stream Pipe
+ *
+ * Ingredients:
+ * 	stream.pipe()
+ *
+ * @config 針對本 task 的 configuration。
+ * @tasks 傳入的子 tasks 為 configurableTask，是尚未綁定 config 的 task 形式。
+ *
+ */
+function pipe() {
+	var i, n, stream;
+
+	var context = this,
+		tasks = context.tasks,
+		runTask = verify(context, true);
+
+	stream = context.upstream;
+	for (i = 0, n = tasks.length; i < n; ++i) {
+		context.upstream = stream;
+		stream = runTask(tasks[i]);
+	}
+	return stream;
+}
+
+pipe.expose = [];
+
+pipe.schema = {
+	"title": "pipe",
+	"description": "",
+	"properties": {
+	}
+};
+
+pipe.type = 'stream';
+
+module.exports = pipe;
